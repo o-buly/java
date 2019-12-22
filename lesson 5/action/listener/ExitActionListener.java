81de10152033a8ca6c7636d75dd51fecdd366442
o-buly <44566428+o-buly@users.noreply.github.com>
2019-12-22 16:02:28 +0200

    Add files via upload

---
+++ lesson 5/action/listener/ExitActionListener.java
@@ -0,0 +1,15 @@
+package action.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ExitActionListener implements ActionListener 
+{
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		System.exit(0);
+	}
+	
+}
